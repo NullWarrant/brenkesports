21fd98b62822a4cc112ea7801f4aefb5b2822a8a
Nathan Brenke <nbrenke@gmail.com>
2026-06-16 23:34:01 -0400

    Initial Next.js setup

---
+++ setup.ps1
@@ -0,0 +1,7 @@
+npx create-next-app@latest . --typescript --tailwind --eslint --app --src-dir --import-alias "@/*" --use-npm --yes
+git init
+git add .
+git commit -m "Initial Next.js setup"
+git branch -M main
+git remote add origin https://github.com/NullWarrant/brenkesports.git
+git push -u origin main
